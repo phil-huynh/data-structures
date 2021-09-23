179d971d4753fe63775de82550d408b85146d068
Phil Huynh <philiphuynh98@gmail.com>
2021-09-23 12:27:22 -0400

    functional shared complete

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = {
+    storage: {},
+    top: 0,
+    bottom: 0,
+  };
+  _.extend(instance, queueMethods);
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.top] = value;
+    this.top++;
+  },
+  dequeue: function() {
+    if (this.top - this.bottom) {
+      var value = this.storage[this.bottom];
+      delete this.storage[this.bottom];
+      this.bottom++;
+      return value;
+    }
+  },
+  size: function() {
+    return this.top - this.bottom;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,19 +2,30 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var instance = {
-    container : [],
-    top : 0
+    storage: {},
+    top: 0
   };
   _.extend(instance, stackMethods);
   return instance;
 };
 
-var stackMethods = {};
-stackMethods.push = function(value) {
-  var currentKey = this.top
-  this[currentKey] = value;
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.top] = value;
+    this.top++;
+  },
 
-  this.top = value + 1;
+  pop: function () {
+    if (this.top > 0) {
+      this.top --;
+      var value = this.storage[this.top];
+      delete this.storage[this.top];
+      return value;
+    }
+  },
+
+  size: function() {
+    return this.top;
+  }
+
 };
-
-
